ce5e51d2a5249609edbbf6f1b52427da26cd18d8
jprochazk <honza.spacir@gmail.com>
2021-03-14 15:25:39 +0100

    on load tooltip = null

--- client/core/game/game.ts
+++ client/core/game/game.ts
@@ -117,7 +117,7 @@ export class Game {
             }
         }
         this.overlay.loading = false;
-        this.overlay.tooltip = "";
+        this.overlay.tooltip = null;
 
         const onClose = () => {
             this.overlay.error = "Connection dropped.";
